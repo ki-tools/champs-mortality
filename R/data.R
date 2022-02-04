23b0b82fc2845a2a780fc0f0d70bb77ef08affea
Ryan Hafen <rhafen@gmail.com>
2022-02-04 13:09:25 -0800

    Update error messages

--- R/data.R
+++ R/data.R
@@ -2,23 +2,24 @@
 #' @param data_path path containing config.yaml and data files
 #' @importFrom assertthat assert_that
 #' @importFrom yaml read_yaml
+#' @importFrom cli format_error
 read_and_validate_data <- function(data_path) {
   assertthat::assert_that(dir.exists(data_path),
-    msg = cli::cli_alert_danger("The 'data_path' provided does not exist: \\
-      {data_path}", wrap = TRUE))
+    msg = cli::format_error(
+      "The 'data_path' provided does not exist: '{data_path}'"))
 
   yaml_path <- file.path(data_path, "config.yaml")
   assertthat::assert_that(file.exists(yaml_path),
-    msg = cli::cli_alert_danger("Configuration file does not exist: \\
-    {yaml_path}", wrap = TRUE))
+    msg = cli::format_error(
+      "Configuration file does not exist: '{yaml_path}'"))
 
   cfg <- yaml::read_yaml(yaml_path)
 
   dff <- setdiff(ds_names, names(cfg))
   dff_str <- paste(dff, collapse = ", ")
   assertthat::assert_that(length(dff) == 0,
-    msg = cli::cli_alert_danger("The following required field(s) are not \\
-      found in config.yaml: {dff_str}", wrap = TRUE))
+    msg = cli::format_error("
+      The following required field(s) are not found in config.yaml: {dff_str}"))
 
   ## champs_analytics_dataset
   ads  <- read_file(data_path, cfg, ads_names, "champs_analytics_dataset")
@@ -41,17 +42,15 @@ read_and_validate_data <- function(data_path) {
 read_file <- function(data_path, cfg, nms, ds_name) {
   path <- file.path(data_path, cfg[[ds_name]])
 
-  message("Reading ", path, "...")
-
   assertthat::assert_that(file.exists(path),
-    msg = cli::cli_alert_danger("The file specified in config.yaml for \\
-      {ds_name} does not exist: {path}", wrap = TRUE))
+    msg = cli::format_error(
+      "The file specified in config.yaml for {ds_name} does not exist: {path}"))
 
   ext <- tools::file_ext(path)
 
   assertthat::assert_that(ext %in% c("csv", "xls", "xlsx"),
-    msg = cli::cli_alert_danger("File must have extension 'csv', 'xlsx', \\
-      or 'xls': {path}", wrap = TRUE))
+    msg = cli::format_error(
+      "File must have extension 'csv', 'xlsx', or 'xls': {path}"))
 
   if (ext == "csv") {
     x <- readr::read_csv(path, show_col_types = FALSE, guess_max = 1e6)
@@ -65,8 +64,10 @@ read_file <- function(data_path, cfg, nms, ds_name) {
   idx <- which(!nms2 %in% names(x))
   vars <- paste0("      ", nms[idx], collapse = "\n")
   assertthat::assert_that(length(idx) == 0,
-    msg = cli::cli_alert_danger(
+    msg = cli::format_error(
       "Expecting variables like the following in {path}:\n{vars}"))
+
+  cli::cli_alert_success(path)
 
   x
 }
