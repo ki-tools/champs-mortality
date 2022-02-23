0883fb71ab67e6f4f9149828b4210c71289f3ba7
Ryan Hafen <rhafen@gmail.com>
2022-02-22 18:19:40 -0800

    Update DSS data structure to take advantage of age/other factor cross tabulations

--- R/utils.R
+++ R/utils.R
@@ -38,3 +38,59 @@ check_valid_vals <- function(x, variable, valid, name, ds_name) {
   cli::cli_alert_success("Checked that values for '{variable}' in the \\
     {ds_name} are correct", wrap = TRUE)
 }
+
+# transform so age is a factor/level instead of its own column
+dss_transform <- function(dss) {
+  dplyr::bind_rows(
+    dss %>% dplyr::select(!"age"),
+    dss %>%
+      dplyr::group_by(
+        .data$site, .data$catchment, .data$age, .data$factor,
+        .data$period_start_year, .data$period_end_year
+      ) %>%
+      dplyr::summarise(n = sum(.data$n), .groups = "drop") %>%
+      dplyr::group_by(
+        .data$site, .data$catchment, .data$age,
+        .data$period_start_year, .data$period_end_year
+      ) %>%
+      dplyr::summarise(n = max(.data$n)) %>%
+      dplyr::mutate(factor = "age", level = .data$age) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(!"age")
+  )
+}
+
+#' Get a list of valid conditions found in the causal chain
+#' @param x an object read in from [read_and_validate_data()]
+#' @export
+valid_conditions <- function(x) {
+  assertthat::assert_that(inherits(x, "champs_processed"),
+    msg = cli::format_error("Data must come from process_data()")
+  )
+
+  tmp <- dplyr::tibble(
+    condition = c(
+      x$ads$ic_champs_group_desc,
+      x$ads$uc_champs_group_desc,
+      x$ads$morbid_cond_01_champs_group_desc,
+      x$ads$morbid_cond_02_champs_group_desc,
+      x$ads$morbid_cond_03_champs_group_desc,
+      x$ads$morbid_cond_04_champs_group_desc,
+      x$ads$morbid_cond_05_champs_group_desc,
+      x$ads$morbid_cond_06_champs_group_desc,
+      x$ads$morbid_cond_07_champs_group_desc,
+      x$ads$morbid_cond_08_champs_group_desc
+    )
+  ) %>%
+  dplyr::filter(!is.na(.data$condition)) %>%
+  dplyr::count(.data$condition) %>%
+  dplyr::arrange(-.data$n) %>%
+  dplyr::mutate("causal chain rank" = seq_len(dplyr::n())) %>%
+  dplyr::select(-c("n"))
+
+  # if (message)
+  #   cli::cli_alert_info("note: a higher rank means the condition was found \\
+  #     more frequently somewhere in the causal chain than lower ranks",
+  #     wrap = TRUE)
+  tmp
+}
