40cf721ef316611bacb5af5a52fc7a24bff0c964
Ryan Hafen <rhafen@gmail.com>
2022-02-28 16:15:41 -0800

    Add causal chain option to functions in has_condition.R and fix bug in searching for conditions

--- R/has_condition.R
+++ R/has_condition.R
@@ -9,8 +9,11 @@
 #' Check to see if an ICD10 regular expression is in the causal chain
 #' @param ads CHAMPS analysis dataset
 #' @param rgx regular expression representing ICD10 codes
+#' @param cc if TRUE: searches across causal chain, if FALSE: searches in
+#' underlying cause only
 #' @export
-has_icd10_cc <- function(ads, rgx) {
+has_icd10 <- function(ads, rgx, cc = TRUE) {
+  if (cc) {
     grepl(rgx, ads$underlying_cause_calc) %or%
     grepl(rgx, ads$immediate_cod) %or%
     grepl(rgx, ads$morbid_condition_01) %or%
@@ -21,21 +24,30 @@ has_icd10_cc <- function(ads, rgx) {
     grepl(rgx, ads$morbid_condition_06) %or%
     grepl(rgx, ads$morbid_condition_07) %or%
     grepl(rgx, ads$morbid_condition_08)
+  } else {
+    grepl(rgx, ads$underlying_cause_calc)
+  }
 }
 
 #' Check to see if a CHAMPS group is in the causal chain
 #' @param ads CHAMPS analysis dataset
 #' @param group string representing a CHAMPS group
+#' @param cc if TRUE: searches across causal chain, if FALSE: searches in
+#' underlying cause only
 #' @export
-has_champs_group_cc <- function(ads, group) {
-  (ads$ic_champs_group_desc == group) %or%
-  (ads$uc_champs_group_desc == group) %or%
-  (ads$morbid_cond_01_champs_group_desc == group) %or%
-  (ads$morbid_cond_02_champs_group_desc == group) %or%
-  (ads$morbid_cond_03_champs_group_desc == group) %or%
-  (ads$morbid_cond_04_champs_group_desc == group) %or%
-  (ads$morbid_cond_05_champs_group_desc == group) %or%
-  (ads$morbid_cond_06_champs_group_desc == group) %or%
-  (ads$morbid_cond_07_champs_group_desc == group) %or%
-  (ads$morbid_cond_08_champs_group_desc == group)
+has_champs_group <- function(ads, group, cc = TRUE) {
+  if (cc) {
+    (ads$ic_champs_group_desc %in% group) %or%
+    (ads$uc_champs_group_desc %in% group) %or%
+    (ads$morbid_cond_01_champs_group_desc %in% group) %or%
+    (ads$morbid_cond_02_champs_group_desc %in% group) %or%
+    (ads$morbid_cond_03_champs_group_desc %in% group) %or%
+    (ads$morbid_cond_04_champs_group_desc %in% group) %or%
+    (ads$morbid_cond_05_champs_group_desc %in% group) %or%
+    (ads$morbid_cond_06_champs_group_desc %in% group) %or%
+    (ads$morbid_cond_07_champs_group_desc %in% group) %or%
+    (ads$morbid_cond_08_champs_group_desc %in% group)
+  } else {
+    ads$uc_champs_group_desc %in% group
+  }
 }
