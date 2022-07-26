6cbce8b1d213281a6a2bc635d12c1e16c075d0da
J Hathaway <hathawayj@byui.edu>
2022-07-26 11:19:22 -0600

    fix to table type check

--- R/table_factor_sig.R
+++ R/table_factor_sig.R
@@ -12,10 +12,11 @@ table_factor_sig_stats <- function(
   print_columns = c("MITS", "non-MITS+DSS-only"),
   percent_digits = 1
 ) {
-  assertthat::assert_that(inherits(tbl, "rate_frac_multi_site"),
-    msg = cli::format_error("'tbl' must come from ",
+  assertthat::assert_that(
+    attributes(mft)$cm_class[1] == "factor_table",
+    msg = cli::format_error(paste0("'tbl' must come from ",
     "mits_factor_tables() or ",
-    "cond_factor_tables()")
+    "cond_factor_tables()"))
   )
 
   spacer <- ""
